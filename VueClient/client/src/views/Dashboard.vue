581e00ff4684ef8497bb45b4881251549af91b17
Agustin-Q <agustin.queija@gmail.com>
2020-10-18 00:51:26 -0300

    adds dashboard logic to check for valid token and display user name

--- VueClient/client/src/views/Dashboard.vue
+++ VueClient/client/src/views/Dashboard.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>📊 Dashboard 📈</h1>
+  <section>
+    <h1>📊 Dashboard 📈</h1>
+    <h2 v-if="user.UserName">🎉Welcome {{user.UserName}}!!🎉</h2>
+  </section>
 </template>
 
 <script>
+const API_URL = 'http://192.168.0.249:3000/api/auth/login';
 export default {
+  data: () => ({
+    user: {},
+  }),
+  mounted() {
+    fetch(API_URL, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    }).then((res) => res.json()).then((result) => {
+      if (result.UserName) {
+        console.log('Response has username, the client has sent valid token');
+        console.log(result);
+        this.user = result;
+      } else {
+        console.log('Response does not have username, client sent invalid token');
+        console.log('Clear token from localStorage and redirect to login');
+        localStorage.removeItem('token');
+        this.$router.push('/login');
+      }
+    });
+  },
 };
 </script>
 
