a760d192d7093f75dccc04d37bce76a726434349
Agustin-Q <agustin.queija@gmail.com>
2020-10-20 20:59:25 -0300

    adds Dashboard button if loged in

--- VueClient/client/src/views/Home.vue
+++ VueClient/client/src/views/Home.vue
@@ -9,9 +9,15 @@
   <p>Created to learn web development! 🤓</p>
   <p class="lead">
     <router-link
+    v-if="!isUserLoggedIn()"
     class="btn btn-primary btn-lg"
     :to="{name:'signup'}"
     role="button">Sign Up</router-link>
+    <router-link
+    v-if="isUserLoggedIn()"
+    class="btn btn-primary btn-lg"
+    :to="{name:'Dashboard'}"
+    role="button">Dashboard</router-link>
   </p>
 </div>
   </div>
@@ -21,5 +27,15 @@
 
 export default {
   name: 'Home',
+  methods: {
+    isUserLoggedIn() {
+      if (localStorage.token) {
+        console.log('User logged in.');
+        return true;
+      }
+      return false;
+    },
+  },
 };
+
 </script>
